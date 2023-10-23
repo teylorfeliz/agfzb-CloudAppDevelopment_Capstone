bd6708c1a3af486ce96ee0c44693b1bffec5803d
Teylor Feliz <teylormanuel@gmail.com>
2023-10-23 15:19:56 -0400

    Second part finished: User Management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,10 +14,11 @@ urlpatterns = [
     # path for contact us view
     path(route="contact", view=views.contact, name='contact'),
     # path for registration
-
+    path(route="signup", view=views.registration_request, name="signup"),
     # path for login
-
+    path(route="login_request", view=views.login_request, name='login_request'),
     # path for logout
+    path(route="logout", view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,33 +18,86 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
 def about(request):
+    '''
+        About Page
+    '''
     context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/about.html', context)
+    return render(request, 'djangoapp/about.html', context)
+
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
 def contact(request):
+    '''
+        Contact Page
+    '''
     context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/contact.html', context)
+    return render(request, 'djangoapp/contact.html', context)
+
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    '''
+        Process login
+    '''
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+        else:
+            # TODO add a message for invalid entries
+            return render(request, 'djangoapp/index.html', context)
+    # TODO might add a message
+    return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    '''
+        Process Logout
+    '''
+    logout(request)
+    return redirect('djangoapp/')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
-# ...
+def registration_request(request):
+    '''
+        Process Registration
+    '''
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        password = request.POST['password']
+        
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} is added as new user to the db".format(username))
+        
+        if not user_exist:
+            user = User.objects.create(username=username, first_name=fname, last_name=lname, password=password)
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+
+
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
@@ -58,4 +111,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
